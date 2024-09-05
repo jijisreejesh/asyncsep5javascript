3e898595bf15f7cce4356c81ed13142f4fcd1006
jiji sreejesh <jijibsc@gmail.com>
2024-09-05 18:14:09 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,15 @@
+//  Write a function getUserData that simulates fetching userdata(name,age,place)from an API 
+//  using a promise and async/await syntax.Use setTimeOut to simulate a 2 second delay in the data fetching process 
+ 
+async function getUserData(){
+    let l=new Promise(resolve=>{
+        setTimeout(()=>{
+        data={
+            name:"anu",age:20,place:"palakkad"
+        }
+        resolve(data)
+    }
+    ),2000})  
+    console.log(await l)    
+}
+getUserData();
